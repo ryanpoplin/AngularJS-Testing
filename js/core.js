556b79a01a19bbc5a8b107becc5a6658196bc901
Byrdann Fox <mobileupstate@gmail.com>
2014-05-09 17:39:26 -0400

    Updates...

--- js/core.js
+++ js/core.js
@@ -2,9 +2,9 @@
 
 // data model...
 var todoModel = {
-	user: "Ryan",
+	user: "ExcepApps",
 	// 'items' collection...
-	items: [{ 
+	/*items: [{ 
 		action: "Buy an awesome book on AngularJS...", 
 		done: true 
 	}, {
@@ -19,11 +19,31 @@ var todoModel = {
 	}, {
 		action: "Master NodeJS...",
 		done: false
-	}]
+	}]*/
 };
 
 // 'todoApp' module...
 var todoApp = angular.module("todoApp", []);
+
+// GET...
+todoApp.run(function($http) {
+	$http.get("todo.json").success(function(data) {
+		todoModel.items = data;
+	});
+});
+
+// Filter...
+todoApp.filter("checkedItems", function() {
+	return function(items, showComplete) {
+		var resultArr = [];
+		angular.forEach(items, function(item) {
+			if (item.done === false || showComplete === true) {
+				resultArr.push(item);
+			}
+		});
+		return resultArr;
+	}
+});
 
 todoApp.controller("ToDoCtrl", function($scope) {
 
@@ -44,5 +64,29 @@ todoApp.controller("ToDoCtrl", function($scope) {
 	$scope.warningLevel = function() {
 		return $scope.incompleteCount() < 3 ? "label-success" : "label-warning";
 	};
+
+	// POST...
+	$scope.addNewItem = function(actionText) {
+		$scope.todo.items.push({
+			action: actionText,
+			done: false
+		});
+	};
+
+	// PUT...
+	// when you click the button, run this method...
+	/*$scope.editItem = function() {
+		$http.get("todo.json").success(	function(data) {
+		todoModel.items = data;
+	});
+	};
+
+	// DELETE...
+	// when you click the button, run this method...
+	$scope.deleteItem = function() {
+		$http.get("todo.json").success(function(data) {
+		todoModel.items = data;
+	});
+	};*/
 
 });
