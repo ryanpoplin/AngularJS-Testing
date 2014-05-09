423e6585ef41748d74f2fa598cbc1b04eeaf2ff3
Byrdann Fox <mobileupstate@gmail.com>
2014-05-08 23:27:21 -0400

    Update...

---
+++ js/core.js
@@ -0,0 +1,12 @@
+var model = {
+	user: "Ryan",
+	items: [{ 
+		actions: "Buy an awesome book on AngularJS...", 
+		done: true 
+	}, {
+		actions: "Find Her...",
+		done: false
+	}]
+};
+
+var todoApp = angular.module("todoApp", []);
