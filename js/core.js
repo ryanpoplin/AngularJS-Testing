ee03c17df547f599ae787fc647ddb2090dc79eb9
Byrdann Fox <mobileupstate@gmail.com>
2014-05-19 05:23:27 -0400

    Updates...

---
+++ js/core.js
@@ -0,0 +1,11 @@
+/*
+ * More info:
+ * http://justifygrid.com/
+ *
+ * Created by:
+ * https://twitter.com/CrocoDillon
+ */
+
+// Opera doesn't like font-size: 0, this changes it to 0.5px :)
+if (window.opera)
+  document.documentElement.className += ' opera';
